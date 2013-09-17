d0c39928058d47b5a0edc87b8d81ca57bc3d4dba
Merlyn Albery-Speyer <merlyn@newrelic.com>
2013-09-17 15:41:29 -0700

    Removed Conjured Mana Cake tests

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -172,38 +172,6 @@ describe "#update_quality" do
       before { pending }
       Given(:name) { "Conjured Mana Cake" }
 
-      context "before the sell date" do
-        Given(:initial_sell_in) { 5 }
-        Then { item.quality.should == initial_quality-2 }
-        Then { item.sell_in.should == initial_sell_in-1 }
-
-        context "at zero quality" do
-          Given(:initial_quality) { 0 }
-          Then { item.quality.should == initial_quality }
-        end
-      end
-
-      context "on sell date" do
-        Given(:initial_sell_in) { 0 }
-        Then { item.quality.should == initial_quality-4 }
-        Then { item.sell_in.should == initial_sell_in-1 }
-
-        context "at zero quality" do
-          Given(:initial_quality) { 0 }
-          Then { item.quality.should == initial_quality }
-        end
-      end
-
-      context "after sell date" do
-        Given(:initial_sell_in) { -10 }
-        Then { item.quality.should == initial_quality-4 }
-        Then { item.sell_in.should == initial_sell_in-1 }
-
-        context "at zero quality" do
-          Given(:initial_quality) { 0 }
-          Then { item.quality.should == initial_quality }
-        end
-      end
     end
   end
 
